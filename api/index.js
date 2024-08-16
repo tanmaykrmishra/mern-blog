bd271fb07bdba1149b5cc39f7a469aa394ccdf0e
Tanmay Mishra <tanmaykumarmishra5@gmail.com>
2024-08-16 16:24:29 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(
@@ -11,8 +11,10 @@ mongoose.connect(
     console.log('Mongodb is connected');
 })
 const app = express();
+app.use(express.json());
 
-app.use('/api/v1', userRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
